202327e3473ee631661d665508d76f0dd7248f77
ChilledCosmonaut <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-02-27 11:31:41 +0100

    Setup basic Grid class

---
+++ code/game/GameLogic/Grid.cpp
@@ -0,0 +1,5 @@
+//
+// Created by GoPJo on 15.02.2022.
+//
+
+#include "Grid.h"
---
+++ code/game/GameLogic/Grid.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <vector>
+
+namespace logic {
+    class Grid {
+    public:
+        Grid();
+    private:
+        std::vector<std::vector<std::vector<int>>>
+    };
+}
