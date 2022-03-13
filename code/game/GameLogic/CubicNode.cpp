385634677341e002100aa49ed595033d4adcd1d4
ChilledCosmonaut <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-03-13 13:11:29 +0100

    Implemented basic logic to check if there are any neighbours around a Node

--- code/game/GameLogic/CubicNode.cpp
+++ code/game/GameLogic/CubicNode.cpp
@@ -1,7 +1,9 @@
-#include "CubicNode.h"
+#include "Grid.h"
 
 namespace logic{
     CubicNode::CubicNode() {
+
+
         neighbours.insert({glm::vec3(1.0f,0.0f,0.0f), nullptr});
         neighbours.insert({glm::vec3(0.0f,1.0f,0.0f), nullptr});
         neighbours.insert({glm::vec3(0.0f,0.0f,1.0f), nullptr});
@@ -10,6 +12,43 @@ namespace logic{
         neighbours.insert({glm::vec3(0.0f,0.0f,-1.0f), nullptr});
     }
 
-    std::vector<Node*> CubicNode::checkForNeighbours() {
+    std::vector<CubicNode *> CubicNode::checkForNeighbours(std::map<glm::vec3, CubicNode> *globalKnowledgeBase,
+                                                           std::set<CubicNode *> *exploredNodes) {
+        std::vector<CubicNode *> frontierNeighbours;
+
+        if (radiusCounter > 0) {
+            for (auto neighbour: neighbours) {
+                auto neighbourDirection = neighbour.first;
+                auto globalNeighbourPosition = positionInGlobalSpace + neighbourDirection;
+                CubicNode *currentNeighbour;
+
+                if (globalKnowledgeBase->count(globalNeighbourPosition) == 0) {
+                    globalKnowledgeBase->insert({globalNeighbourPosition, CubicNode()});
+                    currentNeighbour = &globalKnowledgeBase->at(globalNeighbourPosition);
+                    frontierNeighbours.push_back(currentNeighbour);
+                } else {
+                    currentNeighbour = &globalKnowledgeBase->at(globalNeighbourPosition);
+                    if (exploredNodes->count(currentNeighbour) == 0)
+                        frontierNeighbours.push_back(currentNeighbour);
+                }
+                neighbours[neighbourDirection] = currentNeighbour;
+            }
+        } else {
+            for (auto neighbour: neighbours) {
+                auto neighbourDirection = neighbour.first;
+                auto globalNeighbourPosition = positionInGlobalSpace + neighbourDirection;
+                CubicNode *currentNeighbour = neighbours.at(globalNeighbourPosition);
+
+                if (currentNeighbour != nullptr)
+                    currentNeighbour->SetNeighbourAtDirection(neighbourDirection, this);
+            }
+
+        }
+
+        return frontierNeighbours;
+    }
+
+    void CubicNode::SetNeighbourAtDirection(glm::vec3 directionVector, CubicNode *node) {
+        neighbours[directionVector] = node;
     }
 }
--- code/game/GameLogic/CubicNode.h
+++ code/game/GameLogic/CubicNode.h
@@ -4,19 +4,23 @@
 #include <map>
 #include "glm/vec3.hpp"
 #include "Node.h"
+#include "CubicNode.h"
 
 namespace logic{
-    class CubicNode : public Node{
+    class CubicNode{
     public:
         CubicNode();
 
-        std::vector<Node*> checkForNeighbours() override;
+        std::vector<CubicNode*> checkForNeighbours(std::map<glm::vec3, CubicNode>* globalKnowledgeBase, std::set<CubicNode*>* exploredNodes);
 
         // Initialized in the constructor with a pair of vector and nullptr.
         // Later each neighbour is set correspondingly
         std::map<glm::vec3, CubicNode*> neighbours;
 
+        void SetNeighbourAtDirection(glm::vec3 directionVector, CubicNode* node);
+
     private:
         glm::vec3 positionInGlobalSpace;
+        int radiusCounter;
     };
 }
