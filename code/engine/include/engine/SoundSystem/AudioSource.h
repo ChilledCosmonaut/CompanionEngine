8c31d8ed260d14c743d369d56344b770049d2689
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-05-22 21:11:44 +0200

    Setup constructor and initialisation of wav

--- code/engine/include/engine/SoundSystem/AudioSource.h
+++ code/engine/include/engine/SoundSystem/AudioSource.h
@@ -1,7 +1,24 @@
 #pragma once
 
-namespace Sound{
-    class AudioSource{
+#include <string>
+#include "soloud_wav.h"
+#include "engine/GraphicsEngine/Transform.h"
 
+namespace Sound{
+    /// Provides a wrapper for some simplified spatial audio.
+    class AudioSource{
+    public:
+        /// Creates the AudioSource.
+        /// @param soundPath Defines the filepath for the to be played sound.
+        /// @param transform The global position of the audio source.
+        explicit AudioSource(const std::string& soundPath, Graphics::Transform* transform){
+            sound = SoLoud::Wav();
+            sound.load(soundPath.c_str());
+            sourceTransform = transform;
+        }
+
+    private:
+        SoLoud::Wav sound;
+        Graphics::Transform* sourceTransform;
     };
 }
