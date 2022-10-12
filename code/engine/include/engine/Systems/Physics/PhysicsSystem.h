0f731c63266a5529bf87cc4a8d0badcc9f523259
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-10-12 13:06:18 +0200

    undid change to gravity

--- code/engine/include/engine/Systems/Physics/PhysicsSystem.h
+++ code/engine/include/engine/Systems/Physics/PhysicsSystem.h
@@ -35,7 +35,7 @@ namespace gl3::engine::Physics {
 #endif
 
             physx::PxSceneDesc sceneDesc(mPhysics->getTolerancesScale());
-            sceneDesc.gravity = physx::PxVec3(0.0f, -9.81f, 0.0f);
+            sceneDesc.gravity = physx::PxVec3(0.0f, 0.0f, 0.0f);
             mDispatcher = physx::PxDefaultCpuDispatcherCreate(2);
             sceneDesc.cpuDispatcher	= mDispatcher;
             sceneDesc.filterShader	= physx::PxDefaultSimulationFilterShader;
