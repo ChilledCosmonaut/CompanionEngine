87e7e6b0e3af5408374a36936a2af1a305f5d167
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-10-14 10:39:35 +0200

    Provided some utility functions in the Registry for the reactive system

--- code/engine/include/engine/Systems/ECS/Registry.h
+++ code/engine/include/engine/Systems/ECS/Registry.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../../../../src/EntityComponentSystem/entt.hpp"
+#include "Flags.h"
 
 namespace gl3::engine::Ecs {
 
@@ -8,6 +9,42 @@ namespace gl3::engine::Ecs {
     public:
         static entt::registry& getCurrent(){
             return registry;
+        }
+
+        template<typename Component>
+        static void AddSetupFlag(entt::entity entity) {
+            if (!registry.any_of<Flags::Setup<Component>>(entity))
+                registry.emplace<Flags::Setup<Component>>(entity);
+        }
+
+        template<typename Component>
+        static void RemoveSetupFlag(entt::entity entity) {
+            if (registry.any_of<Flags::Setup<Component>>(entity))
+                registry.remove<Flags::Setup<Component>>(entity);
+        }
+
+        template<typename Component>
+        static void AddUpdateFlag(entt::entity entity) {
+            if (!registry.any_of<Flags::Update<Component>>(entity))
+                registry.emplace<Flags::Update<Component>>(entity);
+        }
+
+        template<typename Component>
+        static void RemoveUpdateFlag(entt::entity entity) {
+            if (registry.any_of<Flags::Update<Component>>(entity))
+                registry.remove<Flags::Update<Component>>(entity);
+        }
+
+        template<typename Component>
+        static void AddDestroyFlag(entt::entity entity) {
+            if (!registry.any_of<Flags::Destroy<Component>>(entity))
+                registry.emplace<Flags::Destroy<Component>>(entity);
+        }
+
+        template<typename Component>
+        static void RemoveDestroyFlag(entt::entity entity) {
+            if (registry.any_of<Flags::Destroy<Component>>(entity))
+                registry.remove<Flags::Destroy<Component>>(entity);
         }
 
     private:
