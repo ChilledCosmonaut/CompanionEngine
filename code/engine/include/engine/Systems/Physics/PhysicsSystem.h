4d5ca9b1b9a2ff280b439b36bee74d213197eda6
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-10-15 18:45:51 +0200

    Moved update function of physics system for the correct visibility

--- code/engine/include/engine/Systems/Physics/PhysicsSystem.h
+++ code/engine/include/engine/Systems/Physics/PhysicsSystem.h
@@ -18,69 +18,6 @@ namespace gl3::engine::Physics {
             throw std::domain_error("PhysicsSystem is not yet created");
         }
 
-        physx::PxPhysics* GetPhysics() {
-            return mPhysics;
-        }
-
-        void AddPhysicsObjects(physx::PxRigidDynamic* rigidBody) {
-            mScene->addActor(*rigidBody);
-        }
-
-        void AddPhysicsObjects(physx::PxRigidStatic* rigidStatic) {
-            mScene->addActor(*rigidStatic);
-        }
-
-        void RemovePhysicsObjects() {
-
-        }
-
-    private:
-
-        PhysicsSystem(){
-            // init physx
-            mFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, mDefaultAllocatorCallback, mDefaultErrorCallback);
-            if (!mFoundation) throw("PxCreateFoundation failed!");
-
-            mToleranceScale.length = 100;        // typical length of an object
-            mToleranceScale.speed = 981;         // typical speed of an object, gravity*1s is a reasonable choice
-
-#if DEBUG
-            mPvd = PxCreatePvd(*mFoundation);
-            physx::PxPvdTransport* transport = physx::PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
-            mPvd->connect(*transport, physx::PxPvdInstrumentationFlag::eALL);
-            mPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *mFoundation, mToleranceScale, true, mPvd);
-#else
-            mPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *mFoundation, mToleranceScale);
-#endif
-
-            physx::PxSceneDesc sceneDesc(mPhysics->getTolerancesScale());
-            sceneDesc.gravity = physx::PxVec3(0.0f, 0.0f, 0.0f);
-            mDispatcher = physx::PxDefaultCpuDispatcherCreate(2);
-            sceneDesc.cpuDispatcher	= mDispatcher;
-            sceneDesc.filterShader	= physx::PxDefaultSimulationFilterShader;
-            mScene = mPhysics->createScene(sceneDesc);
-
-#if DEBUG
-            physx::PxPvdSceneClient* pvdClient = mScene->getScenePvdClient();
-            if(pvdClient)
-            {
-                pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
-                pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
-                pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
-            }
-#endif
-            physicsSystem = this;
-        };
-
-        ~PhysicsSystem() override{
-            mPhysics->release();
-#if DEBUG
-            mPvd->release();
-            mPvdTransporter->release();
-#endif
-            mFoundation->release();
-        }
-
         void Update() {
 
             auto& registry = Ecs::Registry::getCurrent();
@@ -161,6 +98,55 @@ namespace gl3::engine::Physics {
                 Ecs::Registry::AddUpdateFlag<Graphics::Components::Transform>(entity);
             }
         };
+
+    private:
+
+        PhysicsSystem(){
+            // init physx
+            mFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, mDefaultAllocatorCallback, mDefaultErrorCallback);
+            if (!mFoundation) throw("PxCreateFoundation failed!");
+
+            mToleranceScale.length = 100;        // typical length of an object
+            mToleranceScale.speed = 981;         // typical speed of an object, gravity*1s is a reasonable choice
+
+#if DEBUG
+            mPvd = PxCreatePvd(*mFoundation);
+            physx::PxPvdTransport* transport = physx::PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
+            mPvd->connect(*transport, physx::PxPvdInstrumentationFlag::eALL);
+            mPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *mFoundation, mToleranceScale, true, mPvd);
+#else
+            mPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *mFoundation, mToleranceScale);
+#endif
+
+            physx::PxSceneDesc sceneDesc(mPhysics->getTolerancesScale());
+            sceneDesc.gravity = physx::PxVec3(0.0f, 0.0f, 0.0f);
+            mDispatcher = physx::PxDefaultCpuDispatcherCreate(2);
+            sceneDesc.cpuDispatcher	= mDispatcher;
+            sceneDesc.filterShader	= physx::PxDefaultSimulationFilterShader;
+            mScene = mPhysics->createScene(sceneDesc);
+
+#if DEBUG
+            physx::PxPvdSceneClient* pvdClient = mScene->getScenePvdClient();
+            if(pvdClient)
+            {
+                pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+                pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+                pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+            }
+#endif
+            physicsSystem = this;
+        };
+
+        ~PhysicsSystem() override{
+            mPhysics->release();
+#if DEBUG
+            mPvd->release();
+            mPvdTransporter->release();
+#endif
+            mFoundation->release();
+        }
+
+
 
         inline static PhysicsSystem *physicsSystem = nullptr;
 
