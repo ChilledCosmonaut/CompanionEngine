e4353c431f042147a4caef6478fbd175cc942758
ChilledCosmonaut <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-10-27 09:03:59 +0200

    Changed image file names to conform with the enumeration

--- code/engine/include/engine/Systems/Graphics/Components/Skybox.h
+++ code/engine/include/engine/Systems/Graphics/Components/Skybox.h
@@ -64,12 +64,12 @@ namespace gl3::engine::Graphics {
         std::shared_ptr<Graphics::shader> shader = nullptr;
         unsigned int VAO, VBO, texture;
         std::vector<std::string> faces{
-                "AllSky_Space_AnotherPlanet_Cam_3_Right-X.png",
-                "AllSky_Space_AnotherPlanet_Cam_2_Left+X.png",
-                "AllSky_Space_AnotherPlanet_Cam_4_Up+Y.png",
-                "AllSky_Space_AnotherPlanet_Cam_5_Down-Y.png",
-                "AllSky_Space_AnotherPlanet_Cam_0_Front+Z.png",
-                "AllSky_Space_AnotherPlanet_Cam_1_Back-Z.png"
+                "AllSky_Space_AnotherPlanet_Cam_3_Right_minusX.png",
+                "AllSky_Space_AnotherPlanet_Cam_2_Left_plusX.png",
+                "AllSky_Space_AnotherPlanet_Cam_4_Up_plusY.png",
+                "AllSky_Space_AnotherPlanet_Cam_5_Down_minusY.png",
+                "AllSky_Space_AnotherPlanet_Cam_0_Front_plusZ.png",
+                "AllSky_Space_AnotherPlanet_Cam_1_Back_minusZ.png"
         };
     };
 }
