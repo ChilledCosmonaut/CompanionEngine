0a3a5a497b52c594e5cc0c1ed4fab536e0775379
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-11-15 18:19:29 +0100

    Added another virtual function to guarantee that every group has a get function

--- code/engine/include/engine/Systems/Input/InputGroup.h
+++ code/engine/include/engine/Systems/Input/InputGroup.h
@@ -2,8 +2,11 @@
 
 namespace gl3::engine::inputSystem {
 
+    template<typename translatedInput>
     class InputGroup {
     public:
         virtual void UpdateKeys();
+
+        virtual translatedInput GetValue();
     };
 }
