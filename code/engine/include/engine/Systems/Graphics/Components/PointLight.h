67bf05e67d3c87062d772f596f47a082072bed85
ChilledCosmonaut <jonas.schindler@stud-mail.uni-wuerzburg.de>
2023-01-02 23:28:20 +0100

    Fixed false datatypes

--- code/engine/include/engine/Systems/Graphics/Components/PointLight.h
+++ code/engine/include/engine/Systems/Graphics/Components/PointLight.h
@@ -1,15 +1,13 @@
 #pragma once
 
-#include "glm/glm.hpp"
-
 #include "Light.h"
 
 namespace gl3::engine::Graphics {
 
     struct PointLight : Light {
     public:
-        glm::vec3 constant{};
-        glm::vec3 linear{};
-        glm::vec3 quadratic{};
+        float constant{};
+        float linear{};
+        float quadratic{};
     };
 }
