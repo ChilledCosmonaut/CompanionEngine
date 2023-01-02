636133fd3d34379f00c5af583adbcf2a2d47face
ChilledCosmonaut <jonas.schindler@stud-mail.uni-wuerzburg.de>
2023-01-02 21:57:22 +0100

    dir and spot lights are now inside a loop

--- code/assets/shaders/fragmentShader.glsl
+++ code/assets/shaders/fragmentShader.glsl
@@ -77,11 +77,13 @@ void main()
     vec3 result;
 
     // phase 1: Directional lighting
+    for(int i = 0; i < NR_DIR_LIGHTS; i++)
     result = CalcDirLight(dirLight, norm, viewDir);
     // phase 2: Point lights
     for(int i = 0; i < NR_POINT_LIGHTS; i++)
     result += CalcPointLight(pointLights[i], norm, FragPos, viewDir);
     // phase 3: Spot light
+    for(int i = 0; i < NR_SPOT_LIGHTS; i++)
     result += CalcSpotLight(spotLight, norm, FragPos, viewDir);
 
     fragColor = vec4(result, 1.0);
