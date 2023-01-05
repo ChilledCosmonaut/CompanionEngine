3f13b9f0d08464ef8f733faaaa0236bbbd656b30
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2023-01-05 13:25:48 +0100

    Shader does now also consider Normal maps for light calculation

--- code/assets/shaders/fragmentShader.glsl
+++ code/assets/shaders/fragmentShader.glsl
@@ -69,7 +69,9 @@ vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
 void main()
 {
     // properties
-    vec3 norm = normalize(/*TBN * */Normal);
+    vec3 norm = texture(material.normal, TexCoords).rgb;
+    norm = norm * 2.0 - 1.0;
+    norm = normalize(TBN * norm);
     vec3 viewDir = normalize(viewPos - FragPos);
 
     vec3 result = vec3(0.0f, 0.0f, 0.0f);
