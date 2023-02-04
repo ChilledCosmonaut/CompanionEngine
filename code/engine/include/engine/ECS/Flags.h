869f117667acffa122b56f8e6279d407275247a9
ChilledCosmonaut <jonas.schindler@stud-mail.uni-wuerzburg.de>
2023-02-04 11:48:26 +0100

    Added docs for Ecs

--- code/engine/include/engine/ECS/Flags.h
+++ code/engine/include/engine/ECS/Flags.h
@@ -2,14 +2,29 @@
 
 namespace gl3::engine::Ecs::Flags {
 
+    /**
+     * A simple set up flag.
+     * @tparam Component Specified component type
+     */
     template <typename  Component>
     struct Setup {};
 
+    /**
+     * A simple update flag.
+     * @tparam Component Specified component type
+     */
     template <typename  Component>
     struct Update {};
 
+    /**
+     * A simple destroy flag.
+     * @tparam Component Specified component type
+     */
     template <typename  Component>
     struct Destroy {};
 
+    /**
+     * General purpose destroy flag for entity wide destruction.
+     */
     struct DestroyEntity {};
 }
